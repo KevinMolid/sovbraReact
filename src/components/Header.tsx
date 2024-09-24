f2d47a1c7f4ac110c6e518cf6ae3d1634a6c69ff
Kevin Molid <kevinmolid@gmail.com>
2024-09-24 13:46:53 +0200

    Init and Header base

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+const Header = () => {
+  return (
+    <header className="flex justify-between px-24 py-6 items-baseline">
+      <h1 className="font-medium text-2xl text-blue-300 uppercase">
+        Søvnklinikken
+      </h1>
+      <nav>
+        <ul className="flex gap-4 uppercase">
+          <li>Søvnproblemer</li>
+          <li>Behandling</li>
+          <li>Priser</li>
+          <li>Timebestilling</li>
+          <li>Om oss</li>
+          <li>Kontakt</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
