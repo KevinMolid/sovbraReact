0bb9a88477572b13db76023c6b2f1df8387f5365
Kevin Molid <kevinmolid@gmail.com>
2024-09-26 12:47:17 +0200

    Made PosterSection for Problems page

--- src/pages/Problems.tsx
+++ src/pages/Problems.tsx
@@ -1,9 +1,68 @@
 import HeaderSection from "../components/HeaderSection";
+import PosterSection from "../components/PosterSection";
+import PosterBox from "../components/PosterBox";
 
 const Problems = () => {
   return (
     <main>
       <HeaderSection>Søvnproblemer</HeaderSection>
+      <PosterSection heading="Vanlige årsaker til søvnproblemer">
+        <PosterBox heading="Stress" icon="chart-column">
+          <p>
+            Stresshormonet kortisol gjør oss våken og fokusert. Når det er for
+            mye av dette blir det vanskelig å sove.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Pust" icon="wind">
+          <p>
+            Tett nese og problemer med muskulatur og slimhinner i halsen kan
+            redusere oksygeninntaket og føre til dårlig søvnkvalitet.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Hormoner" icon="street-view">
+          <p>
+            Overgangsalder, menstruasjonsplager, graviditet, stoffskifte og
+            andre hormonelle forandringer kan forstyrre søvnmønsteret vårt.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Mage / Tarm" icon="poop">
+          <p>
+            Problemer i fordøyelsen og organsystemet kan føre til oppvåkninger
+            og uro i kroppen.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Immunforsvar" icon="viruses">
+          <p>
+            Et overaktivt immunsystem skaper uro i kroppen. Mange kan få
+            problemer med søvnen i etterkant av store infeksjoner.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Muskulatur" icon="person-walking">
+          <p>
+            Spenninger i kroppen kan gjøre det vanskelig å slappe av og finne
+            rett sovestilling.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Tankekjør" icon="brain">
+          <p>
+            Når tankene spinner ukontrollert klarer ikke hjernen å gå over i
+            nattmodus.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Koffein" icon="mug-hot">
+          <p>
+            Kaffe, brus og te med koffein gjør at vi bruker lengre tid på å
+            sovne og at vi får mindre dyp søvn.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Skjerm" icon="mobile-screen-button">
+          <p>
+            Hjernen må jobbe mye for å prosessere all informasjon som kommer fra
+            skjermen. Blått lys fra skjermer kan også lure hjernen vår til å tro
+            at det er dag.
+          </p>
+        </PosterBox>
+      </PosterSection>
     </main>
   );
 };
--- src/pages/Treatment.tsx
+++ src/pages/Treatment.tsx
@@ -1,12 +1,54 @@
 import HeaderSection from "../components/HeaderSection";
 import CtaSection from "../components/CtaSection";
 import PosterSection from "../components/PosterSection";
+import PosterBox from "../components/PosterBox";
+import TextSection from "../components/TextSection";
 
 const Treatment = () => {
   return (
     <main>
       <HeaderSection>Behandling for søvnproblemer</HeaderSection>
-      <PosterSection />
+      <PosterSection heading="SØVNPROBLEMER VI JOBBER MED">
+        <PosterBox heading="Søvnløshet / insomni" icon="eye">
+          <p>
+            Søvnløshet (Insomni) kjennetegnes av problemer med å sovne, hyppige
+            oppvåkninger på natten, tidlig oppvåkning om morgenen, samt redusert
+            funksjonsevne på dagtid.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Døgnrytme" icon="cloud-moon">
+          <p>
+            Døgnrytmeforstyrrelser oppstår når kroppens indre klokke ikke er i
+            takt med det ytre miljøet, noe som fører til utfordringer med riktig
+            timing av søvnen.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Småbarn" icon="baby-carriage">
+          <p>
+            Søvn er avgjørende for barns utvikling, deres psykiske helse,
+            atferd, samt evnen til læring og konsentrasjon.
+          </p>
+        </PosterBox>
+        <PosterBox heading="snorking / søvnapné" icon="wind">
+          <p>
+            Både snorking og søvnapné (pustestopp) oppstår ved helt eller delvis
+            blokkering av luftveiene under søvn.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Bevegelseforstyrrelser" icon="shoe-prints">
+          <p>
+            Urolige bein eller muskelspenninger kan gjøre det utfordrende å
+            sovne, bidra til oppvåkninger eller urolig søvn.
+          </p>
+        </PosterBox>
+        <PosterBox heading="Parasomnier" icon="magnifying-glass">
+          <p>
+            Parasomnier er unormale atferdsmønstre eller fenomener som oppstår
+            under søvn, som natteskrekk, søvngjengeri, og mareritt.
+          </p>
+        </PosterBox>
+      </PosterSection>
+      <TextSection></TextSection>
       <CtaSection />
     </main>
   );
